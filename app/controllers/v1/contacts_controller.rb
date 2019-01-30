3e6b7973d43a55c1a05b467ecd3ffd4cf56eaf3c
Shawn Kearney <shawnpk@ineptsoftware.com>
2019-01-30 16:45:54 -0700

    Add CRUD actions to contacts_controller.rb

--- app/controllers/v1/contacts_controller.rb
+++ app/controllers/v1/contacts_controller.rb
@@ -1,26 +1,28 @@
-class V1::ContactsController < ApplicationController
-  def index
-    contacts = Contact.all
+module V1
+  class ContactsController < ApplicationController
+    def index
+      contacts = Contact.all
 
-    render json: contacts, status: :ok
-  end
-
-  def create
-    contact = Contact.new(contact_params)
-    contact.save!
-
-    render json: contact, status: :created
-  end
-
-  def destroy
-    contact = Contact.find(params[:id])
-    contact.destroy!
-
-    head :no_content
-  end
-
-  private
-    def contact_params
-      params.require(:contact).permit(:first_name, :last_name, :email)
+      render json: contacts, status: :ok
     end
+
+    def create
+      contact = Contact.new(contact_params)
+      contact.save!
+
+      render json: contact, status: :created
+    end
+
+    def destroy
+      contact = Contact.find(params[:id])
+      contact.destroy!
+
+      head :no_content
+    end
+
+    private
+      def contact_params
+        params.require(:contact).permit(:first_name, :last_name, :email)
+      end
+  end
 end
