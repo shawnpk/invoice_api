3bfbb38d2d33fcf4d01de8ff844f168f0505dcda
Shawn Kearney <shawnpk@ineptsoftware.com>
2019-01-30 12:36:44 -0700

    Add create, destroy and contact params methods

--- app/controllers/v1/contacts_controller.rb
+++ app/controllers/v1/contacts_controller.rb
@@ -4,4 +4,23 @@ class V1::ContactsController < ApplicationController
 
     render json: contacts, status: :ok
   end
+
+  def create
+    contact = Contact.new(contact_params)
+    contact.save!
+
+    render json: contact, status: :created
+  end
+
+  def destroy
+    contact = Contact.find(params[:id])
+    contact.destroy!
+
+    head :no_content
+  end
+
+  private
+    def contact_params
+      params.require(:contact).permit(:first_name, :last_name, :email)
+    end
 end
