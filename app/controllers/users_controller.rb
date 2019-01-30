8f38f2bce1ca5b1bc131f514420980486f8bf7a6
Shawn Kearney <shawnpk@ineptsoftware.com>
2019-01-30 16:44:12 -0700

    Add CRUD user actions and user routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,52 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update destroy]
+
+  def index
+    users = User.all
+
+    render json: users
+  end
+
+  def show
+    render json: @user
+  end
+
+  def new
+    user = User.new(user_params)
+  end
+
+  def edit
+  end
+
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      render json: user, status: :created
+    else
+      render :new
+    end
+  end
+
+  def update
+    @user.update(user_params)
+
+    if @user.save
+      render @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+
+    # head :no_content
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :users
+
   namespace :v1 do
     resources :contacts
     resources :sessions, only: %i[create destroy]
