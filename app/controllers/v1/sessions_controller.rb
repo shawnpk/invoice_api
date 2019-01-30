e844f3dbbe171afbeb7097af14595ee2d6cbdfc3
Shawn Kearney <shawnpk@ineptsoftware.com>
2019-01-30 16:46:17 -0700

    Add create and destroy actions to sessions_controller.rb

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -1,9 +1,17 @@
-class SessionsController < ApplicationController
-  def create
-    
-  end
+module V1
+  class SessionsController < ApplicationController
+    def create
+      @user = User.find_by!(email: params[:email])
 
-  def destroy
+      if @user&.authenticate(params[:password])
+        render json: @user.as_json(only: %i[id]), status: :created
+      else
+        head :unauthorized
+      end
+    end
 
+    def destroy
+
+    end
   end
 end
