fb28b4c58a8f10a596693d98fbadcfbbb1f90952
Shawn Kearney <shawnpk@ineptsoftware.com>
2019-01-31 06:46:54 -0700

    Add destroy method body

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -1,5 +1,9 @@
 module V1
   class SessionsController < ApplicationController
+    def show
+      current_user ? head :ok : head :unauthorized
+    end
+
     def create
       @user = User.find_by!(email: params[:email])
 
@@ -11,7 +15,13 @@ module V1
     end
 
     def destroy
-      
+      current_user&.authenticate_token = nil
+
+      if current_user.save
+        head :ok
+      else
+        head :unauthorized
+      end
     end
   end
 end
